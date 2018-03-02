c4563c6ed74420992339e02f08cd12ca2011afc3
Sir Ynoty <mantony0148@icloud.com>
2018-03-02 13:30:03 -0300

    bd

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Lotus
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.assets.initialize_on_precompile = false
+
+
     config.middleware.use Rack::Cors do
       allow do
         origins '*'
